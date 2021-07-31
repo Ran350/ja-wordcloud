5cc4e23be25c6c5be17ff0676ac49e7336925e0a
Hoshi Ran <ureshinostyle350@gmail.com>
2021-07-31 18:29:02 +0900

    [add] rust project for wasm by cargo-generate

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename =
+      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
+    return config;
+  },
+};
---
+++ src/lib/tokenizer/src/lib.rs
@@ -0,0 +1,16 @@
+mod utils;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn tokenize(value: i32) -> i32 {
+    let mut a: i32 = 0;
+    for i in 1..value+1 {
+        a += i;
+    }
+    a
+}
