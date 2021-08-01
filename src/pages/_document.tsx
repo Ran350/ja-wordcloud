914ad66f00024198d3e31dac017a2711ce40238d
Hoshi Ran <ureshinostyle350@gmail.com>
2021-08-02 01:14:40 +0900

    [add] page info

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,10 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <meta charSet="UTF-8" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>☁️</text></svg>"
+          />
         </Head>
 
         <body className="">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { FC, useState } from "react";
+import Head from "next/head";
+
 import { WC } from "../components/WC";
 
 const Token: FC = () => {
@@ -17,9 +19,15 @@ const Token: FC = () => {
 
   return (
     <div>
+      <Head>
+        <title>Wordcloud</title>
+      </Head>
+
       <input size={100} type="text" onChange={(e) => handleOnChange(e)} />
+
       <h1>origin: {text}</h1>
       <h1>token: {token}</h1>
+
       <WC token={token} />
     </div>
   );
