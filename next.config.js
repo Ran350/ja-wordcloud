bec9caf4475b77388f70ec02c237b1c9c97bee35
Hoshi Ran <ureshinostyle350@gmail.com>
2021-08-07 00:13:08 +0900

    [add] icons for pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
   assetPrefix: "/web-wordcloud",
+  basePath: "/web-wordcloud",
 
   pwa: {
     dest: "public",
+    runtimeCaching,
+    scope: "/web-wordcloud",
     disable: process.env.NODE_ENV === "development",
   },
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import App from "next/app";
-import { AppProps, AppContext } from "next/app";
+import App, { AppContext, AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <meta name="theme-color" content="#285943" />
+        <link rel="manifest" href="manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="icons/icon192.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
