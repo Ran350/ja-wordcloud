88e792cb7bd2cacfa89d938f5215726abd547a08
Hoshi Ran <ureshinostyle350@gmail.com>
2021-08-18 15:23:59 +0900

    [add] tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import App, { AppContext, AppProps } from "next/app";
 import Head from "next/head";
 
+import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
