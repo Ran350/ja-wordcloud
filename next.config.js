1a26351d33af982ad7cb3b2b7b708017e2dd31fd
Hoshi Ran <ureshinostyle350@gmail.com>
2022-03-21 22:44:27 +0900

    [change] url when develop

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')
 
-const urlPrefix = '/web-wordcloud'
+const isProd = process.env.NODE_ENV === 'production'
+const urlPrefix = isProd ? '/web-wordcloud' : ''
 
 module.exports = withPWA({
   reactStrictMode: true,
+
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
   publicRuntimeConfig: { urlPrefix },
+
+  optimizeFonts: false,
 
   pwa: {
     dest: 'public',
