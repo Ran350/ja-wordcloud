fc02d3f26ed78ea131d6fe59a23d4c5ef8f82e11
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-03 01:26:32 +0900

    [fix] import error in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
