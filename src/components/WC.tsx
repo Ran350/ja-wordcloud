d4105956aea39e997605fd1a6f2fce018ab378d7
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-03 01:28:12 +0900

    [change] make <WC/> immutable

--- src/components/WC.tsx
+++ src/components/WC.tsx
@@ -1,45 +1,31 @@
 import { useEffect, useRef } from 'react'
-import TinySegmenter from 'tiny-segmenter'
 
-import { tokenToListEntry } from '../lib/format/format'
+import { generateWCOption } from 'lib/getWCOption'
+import { generateWCToken } from 'lib/generateWCToken'
 
-import type { Form } from 'src/lib/form/index.type'
-import { translateForm2Option as translateForm2Options } from 'src/lib/translateForm2Option'
+import type { Form } from 'types/form.type'
 
 type Props = {
   sentence: string
-  form: Form
-  ranCount: number
+  stopWordList: string[]
+  wcStyleOption: Form
+  magnification: number
 }
-export const WC: React.VFC<Props> = ({ sentence, form, ranCount }) => {
+export const WC: React.VFC<Props> = ({ sentence, stopWordList, wcStyleOption, magnification }) => {
   const ref = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    console.time('all')
-
-    /* word segmentation */
-    console.time('seg')
-    const segmenter = new TinySegmenter()
-    const segments = segmenter.segment(sentence)
-    console.timeEnd('seg')
-
-    /* remove stop words */
-    const words = tokenToListEntry(segments)
-
-    /* create word cloud */
+    const token = generateWCToken(sentence, stopWordList)
     import('wordcloud').then(({ default: WordCloud }) => {
       // 動的importしているのは、wordcloud2.jsがwindow objectを参照しており、Next.jsがSSG/SSRできないため
-      const options = translateForm2Options(form)
       if (ref.current === null) return
-      WordCloud(ref.current, { list: words, ...options })
+      WordCloud(ref.current, { list: token, ...generateWCOption(wcStyleOption) })
     })
-
-    console.timeEnd('all')
-  }, [ranCount]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [sentence, wcStyleOption, stopWordList])
 
   return (
     <>
-      <canvas ref={ref} id="canvas" width={16 * 40} height={9 * 40}></canvas>
+      <canvas ref={ref} id="canvas" width={16 * magnification} height={9 * magnification}></canvas>
     </>
   )
 }
