97c1343bcebd1f75d3cf67fe8d327e238c649862
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-03 06:32:28 +0900

    [delete] config.ts

--- src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-import getConfig from 'next/config'
-
-export const url = (filename: string): string => {
-  const { publicRuntimeConfig } = getConfig() as {
-    publicRuntimeConfig: { urlPrefix: string }
-  }
-  return publicRuntimeConfig.urlPrefix + filename
-}
