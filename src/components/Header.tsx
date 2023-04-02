62a938f9323d268cdd5521270912d7fde84214f2
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-03 01:25:59 +0900

    [refactor] <Header/>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import { Anchor, Flex, Header as MHeader, Title } from '@mantine/core'
+import { ActionIcon, Flex, Image, Header as MHeader, Title } from '@mantine/core'
 
-import { url } from 'src/utils/config'
+import { url } from 'utils/config'
 
 export const Header: React.VFC = () => {
   const githubURL = 'https://github.com/ran350/ja-wordcloud'
-  const githubIconURL = githubURL + url('/icons/GitHub.svg')
+  const githubIconSrc = url('/icons/GitHub.svg')
 
   return (
     <MHeader height={60} p="xs">
-      <Flex align="center" justify="space-between">
-        <Title order={2}>Japanese Word Cloud</Title>
-        <Anchor href={githubURL} target="_blank">
-          <img src={githubIconURL} alt="GitHub" />
-        </Anchor>
+      <Flex justify="space-between" align="center">
+        <Title order={3}>Japanese Word Cloud</Title>
+        <ActionIcon variant="outline" size="lg" p={3} component="a" href={githubURL}>
+          <Image src={githubIconSrc} />
+        </ActionIcon>
       </Flex>
     </MHeader>
   )
