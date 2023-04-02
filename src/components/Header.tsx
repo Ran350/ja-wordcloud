f5b5ce9da3716bf8546b3d19ca5f6c2d4d74c348
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-03 03:03:26 +0900

    [feat] <GitHubIcon/>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import { ActionIcon, Flex, Image, Header as MHeader, Title } from '@mantine/core'
+import { Anchor, Flex, Header as MHeader, Title } from '@mantine/core'
 
-import { url } from 'utils/config'
+import { GithubIcon } from './icon/GitHubIcon'
 
 export const Header: React.VFC = () => {
-  const githubURL = 'https://github.com/ran350/ja-wordcloud'
-  const githubIconSrc = url('/icons/GitHub.svg')
-
   return (
     <MHeader height={60} p="xs">
       <Flex justify="space-between" align="center">
-        <Title order={3}>Japanese Word Cloud</Title>
-        <ActionIcon variant="outline" size="lg" p={3} component="a" href={githubURL}>
-          <Image src={githubIconSrc} alt="github" />
-        </ActionIcon>
+        <Title order={3}>
+          <Anchor href="https://ran350.github.io/ja-wordcloud/" color="gray.7">
+            Japanese Word Cloud
+          </Anchor>
+        </Title>
+        <Anchor href="https://github.com/ran350/ja-wordcloud"></Anchor>
+        <GithubIcon size={30} />
       </Flex>
     </MHeader>
   )
