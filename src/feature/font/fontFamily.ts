9cdb66a177532d5bdc155fb62aac389028e58026
Hoshi Ran <ureshinostyle350@gmail.com>
2023-04-09 19:28:24 +0900

    [feat] feature/font

---
+++ src/feature/font/fontFamily.ts
@@ -0,0 +1,22 @@
+type FontFamily = {
+  name: string
+  fallbacks: string[]
+}
+export const fontFamilies: FontFamily[] = [
+  { name: 'Dela Gothic One ', fallbacks: ['cursive'] },
+  { name: 'DotGothic16', fallbacks: ['sans-serif'] },
+  { name: 'Hachi Maru Pop', fallbacks: ['cursive'] },
+  { name: 'Kaisei Tokumin', fallbacks: ['serif'] },
+  { name: 'Kiwi Maru', fallbacks: ['serif'] },
+  { name: 'M PLUS 1', fallbacks: ['sans-serif'] },
+  { name: 'M PLUS Rounded 1c', fallbacks: ['sans-serif'] },
+  { name: 'Mochiy Pop One', fallbacks: ['sans-serif'] },
+  { name: 'Noto Sans JP', fallbacks: ['sans-serif'] },
+  { name: 'Noto Serif JP', fallbacks: ['serif'] },
+  { name: 'Potta One', fallbacks: ['cursive'] },
+  { name: 'Rampart One', fallbacks: ['cursive'] },
+  { name: 'RocknRoll One', fallbacks: ['sans-serif'] },
+  { name: 'Sawarabi Mincho', fallbacks: ['serif'] },
+  { name: 'Train One', fallbacks: ['cursive'] },
+  { name: 'Yuji Syuku', fallbacks: ['serif'] },
+]
